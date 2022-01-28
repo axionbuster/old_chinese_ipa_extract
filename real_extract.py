981cf5da06ffbd6fabe56674714631d6d3c00f1b
YuJin Kim <yujinkim@mines.edu>
2022-01-28 12:14:48 -0700

    Reformat using Black

--- real_extract.py
+++ real_extract.py
@@ -20,7 +20,7 @@ def http() -> requests.Session:
 # Download the HTTP file from Wiktionary
 def wikt(sesh: requests.Session, word: str) -> BeautifulSoup:
     response = sesh.get(f"https://en.wiktionary.org/wiki/{word}?printable=true")
-    soup = BeautifulSoup(response.text.replace('>\n<', '><'), 'html.parser')
+    soup = BeautifulSoup(response.text.replace(">\n<", "><"), "html.parser")
     return soup
 
 
@@ -29,34 +29,35 @@ def zhengzhang(soup: BeautifulSoup) -> [str]:
     try:
         # Identify a parent div whose descendant contains the text "Old Chinese"
         # (The parent div is specified)
-        parent = soup.select('#mw-content-text > div.mw-parser-output > div.toccolours.zhpron li')
+        parent = soup.select(
+            "#mw-content-text > div.mw-parser-output > div.toccolours.zhpron li"
+        )
         child = filter(lambda entry: "Old Chinese" in entry.text, parent)
 
         # Only choose those whose descendants contain the string "Zhengzhang"
         children = map(
-            lambda dd:
-                list(filter(
-                    lambda entry:
-                        'Zhengzhang' in entry.text,
-                    dd.select('dd'))),
-            child)
+            lambda dd: list(
+                filter(lambda entry: "Zhengzhang" in entry.text, dd.select("dd"))
+            ),
+            child,
+        )
 
         # Now, flatten everything and filter the text
         def flatten(t):
             return [item for sublist in t for item in sublist]
 
-        ipas = map(lambda e: e.select('span.IPAchar'), flatten(children))
+        ipas = map(lambda e: e.select("span.IPAchar"), flatten(children))
         ipas = flatten(map(lambda t: list(map(lambda e: str(e.text), t)), ipas))
 
         # Separate by comma and then remove spaces
-        ipas = flatten(map(lambda s: s.split(','), ipas))
+        ipas = flatten(map(lambda s: s.split(","), ipas))
         ipas = map(lambda s: s.strip(), ipas)
 
         # Remove "unattested/reconstruction" symbols /* and /
         ipas = map(lambda e: e[2:-1], ipas)
 
         # Normalize
-        ipas = map(lambda e: unicodedata.normalize('NFC', e), ipas)
+        ipas = map(lambda e: unicodedata.normalize("NFC", e), ipas)
 
         # Export
         return list(ipas)
@@ -67,14 +68,16 @@ def zhengzhang(soup: BeautifulSoup) -> [str]:
 
 # Suppose stdin contains the chinese characters line by line.
 # Many characters per line
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Help: Line-by-line operation.")
     print("Help: Will print the possible pronunciations for each Chinese character")
     print("Help: And then, machine will convert the entire line into IPA pronunciation")
     print("Help: Formats: ")
     print("Help: [Line|Pronounce|IPA|Ignoring] %d: %s")
     print("Help: %d = line number from 1, %s = message")
-    print("Help: Will also output a dictionary as cache; CLEARED upon OPENING! BACK IT UP!")
+    print(
+        "Help: Will also output a dictionary as cache; CLEARED upon OPENING! BACK IT UP!"
+    )
 
     line_no = 1
     ipa_cache = dict()
@@ -86,7 +89,7 @@ if __name__ == '__main__':
     except FileNotFoundError:
         pass
     finally:
-        open("dict.json", 'a').close()
+        open("dict.json", "a").close()
 
     try:
         with open("dict.json", "r") as ipa_cache_file:
@@ -94,14 +97,16 @@ if __name__ == '__main__':
                 ipa_cache = json.load(ipa_cache_file)
                 print("`dict.json` was successfully parsed; thank you.")
             except:
-                print("No dictionary `dict.json` was found or it was unusable; it's a-ok.")
+                print(
+                    "No dictionary `dict.json` was found or it was unusable; it's a-ok."
+                )
             finally:
                 ipa_cache_file = open("dict.json", "w")
 
             for line in sys.stdin:
                 try:
                     # First, normalize the line according to Composition
-                    unicodedata.normalize('NFC', line)
+                    unicodedata.normalize("NFC", line)
 
                     # Get rid of newline
                     line = line[:-1]
@@ -134,7 +139,7 @@ if __name__ == '__main__':
                 except KeyboardInterrupt:
                     break
                 except Exception:
-                    print(f"Ignoring {line_no}: \"{line}\"")
+                    print(f'Ignoring {line_no}: "{line}"')
                     traceback.format_exc()
                     pass
                 finally:
